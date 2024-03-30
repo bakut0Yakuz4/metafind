2d06950af55bf4a9d150cb5dbb5013e4d862bd72
Myacheep <71332923+bakut0Yakuz4@users.noreply.github.com>
2024-03-29 22:15:19 -0300

    Add files via upload

---
+++ metafind.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+site="$1"
+ext="$2"
+
+lynx --dump "https://google.com/search?&q=site:$site+ext:$ext" | grep ".$ext" | cut -d "=" -f2 | egrep -v "site|google" | sed 's/...$//' > result.txt
+
+
+for url in $(cat result.txt);do wget -q $url;done
+
+for sep in $(cat result.txt);do echo $sep | cut -d "/" -f5 >> result2.txt ;done
+
+for meta in $(cat result2.txt);do exiftool $meta;done
